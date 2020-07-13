5c40e23339675c37e4dd0cb05cb2ef6a90a2c485
shimotaroo <shimotaro.0327@gmail.com>
2020-07-13 23:50:34 +0900

    create login function and login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,45 @@
+@extends('app')
+
+@section('title', 'ログイン')
+
+@section('content')
+
+    @include('nav')
+
+    <div class="row">
+        <div class="mx-auto col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+            <div class="card mt-5">
+                <div class="card-body text-center">
+                    <h2 class='h3 card-title text-center mt-2'>ログイン</h2>
+                    <small>sign in</small>
+
+                    @include('error_card_list')
+
+                    <div class="card-text">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="md-form">
+                                <label for="email">メールアドレス</label>
+                                <input type="text" class="form-control" id="email" name="email" required value="{{ old('email') }}">
+                            </div>
+                            <div class="md-form">
+                                <label for="password">パスワード</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                                <small>8文字以上で入力してください</small>
+                            </div>
+                            <input type="hidden" name="remember" id="remember" value="on">
+                            <button class="btn btn-block btn-outline-info waves-effect mt-2 mb-2" type="submit">
+                                ログイン
+                            </button>
+                        </form>
+
+                        <div class="mt-4">
+                            <a class="card-text" href="{{ route('register') }}">新規登録はこちら</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
