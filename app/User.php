d688cbb7e02aac3d151d687f5a3aa04d9b4f8c95
shimotaroo <shimotaro.0327@gmail.com>
2020-07-28 00:23:04 +0900

    cretate profile-image upload

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'age', 'email', 'password',
+        'name', 'age', 'email', 'password','image',
     ];
 
     /**
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,32 +14,48 @@
 
                 @include('error_card_list')
 
-                <div class="mt-5">
-
-                        <div class="card-body align-items-center mt-2 mb-3">
-                            <form action="{{ route('users.update', ['name' => $user->name]) }}" method="POST">
+                <div class="mt-2">
+                        <div class="card-body align-items-center text-center mt-2 mb-3">
+                            <form action="{{ route('users.update', ['name' => $user->name]) }}" method="POST" enctype="multipart/form-data">
                                 @method('PATCH')
                                 @csrf
                                 {{-- 編集フォーム --}}
-                                <div class="ml-4">
-                                    <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-                                        <img src="{{ asset('storage/'.$user->image) }}" alt="Contact Person" class="img-fuild rounded-circle" width="60" height="60">
-                                    </a>
+                                <label for="image">
+                                    <img src="{{ asset('storage/'.$user->image) }}" id="img" class="img-fuild rounded-circle" width="80" height="80">
+                                    <input type="file" id="image" name="image" onchange="previewImage(this);" class="d-none">
+                                </label>
+                                <div class="md-form col-lg-6 col-md-7 col-sm-8 col-xs-10 mx-auto">
+                                    <label for="name">ユーザー名</label>
+                                    <input type="text" class="form-control" id="name" name="name" required value="{{ $user->name }}">
+                                    <small>3〜15文字で入力してください</small>
                                 </div>
-                                <h2 class="h5 card-title ml-3 mb-0">
-                                    <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-                                        {{ $user->name }}
-                                    </a>
-                                </h2>
-                                <button type="submit" class="btn btn-block cyan darken-3 text-white col-lg-8 col-md-9 col-sm-10 col-xs-12 mx-auto mt-3 mb-5 waves-effect">
+                                <div class="md-form col-lg-6 col-md-7 col-sm-8 col-xs-10 mx-auto">
+                                    <label for="age">年齢</label>
+                                    <input type="text" class="form-control" id="age" name="age" value="{{ $user->age }}">
+                                </div>
+                                <div class="md-form col-lg-6 col-md-7 col-sm-8 col-xs-10 mx-auto">
+                                    <label for="email">メールアドレス</label>
+                                    <input type="text" class="form-control" id="email" name="email" required value="{{ $user->email }}">
+                                </div>
+                                <button type="submit" class="btn btn-block cyan darken-3 text-white col-lg-8 col-md-9 col-sm-10 col-xs-12 mx-auto mt-5 mb-5 waves-effect">
                                     更新する
                                 </button>
                             </form>
                         </div>
-
                 </div>
             </div>
         </div>
     </div>
     @include('footer')
 @endsection
+
+<script>
+    function previewImage(obj)
+    {
+        var fileReader = new FileReader();
+        fileReader.onload = (function() {
+            document.querySelector('#img').src = fileReader.result;
+        });
+        fileReader.readAsDataURL(obj.files[0]);
+    }
+</script>
