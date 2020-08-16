9a730bfaf1bf8432290660ff3c364b669d31c147
shimotaroo <shimotaro.0327@gmail.com>
2020-08-16 23:57:56 +0900

    try test

--- config/database.php
+++ config/database.php
@@ -93,11 +93,11 @@ return [
 
         'circle_test' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'circle_test',
+            'username' => 'root',
+            'password' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
