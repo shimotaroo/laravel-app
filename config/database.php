9c1c953bb3cea2d6e1a7431a1e5e74025ba5f948
shimotaroo <shimotaro.0327@gmail.com>
2020-08-18 08:11:56 +0900

    try test

--- config/database.php
+++ config/database.php
@@ -94,7 +94,7 @@ return [
         'circle_test' => [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
-            'port' => '3307',
+            'port' => '3306',
             'database' => 'circle_test',
             'username' => 'root',
             'password' => '',
