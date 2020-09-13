23673c12678408daebbdf0e47c89c10f199f56cb
shimotaroo <shimotaro.0327@gmail.com>
2020-09-13 23:28:21 +0900

    article-show-page image

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -8,7 +8,6 @@ use App\Phase;
 use App\Prefecture;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ArticleControllerTest extends TestCase
