5ebd1fe20933031100c084d35c7670ddacda711f
shimotaroo <shimotaro.0327@gmail.com>
2020-09-27 14:16:03 +0900

    #45 エラーメッセージの表示幅変更

--- resources/views/error_card_list.blade.php
+++ resources/views/error_card_list.blade.php
@@ -1,5 +1,5 @@
 @if ($errors->any())
-    <div class="card-text text-left alert alert-danger">
+    <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 mx-auto card-text text-left alert alert-danger">
         <ul class="mb-0">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
