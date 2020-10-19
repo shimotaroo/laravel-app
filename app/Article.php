ef47d1bd916ed366fed9e0abd152840477ae3bcb
shimotaroo <shimotaro.0327@gmail.com>
2020-10-20 01:01:09 +0900

    #41 リファクタリング（ArticleController）

--- app/Article.php
+++ app/Article.php
@@ -5,6 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Phase;
+use App\Prefecture;
+use App\User;
 
 class Article extends Model
 {
@@ -68,5 +71,47 @@ class Article extends Model
         } elseif ($sort_type === 'like_count') {
             return $this->withCount('likes')->orderBy('likes_count', 'desc');
         }
+    }
+
+    //絞り込み検索時にDBからデータを取得するqueryを作成
+    public function makeQueryOfSearch($query, $prefecture_serch, $company_search, $phase_search)
+    {
+        if($prefecture_serch !== "0") {
+            $query->where('prefecture_id', $prefecture_serch);
+        }
+        if($company_search  !== "0") {
+            $query->where('company_type_id', $company_search );
+        }
+        if($phase_search !== "0") {
+            $query->where('phase_id', $phase_search);
+        }
+
+        return $query;
+    }
+
+    //検索条件の内容を取得
+    public function getSearchConditions($prefecture_search, $company_search, $phase_search)
+    {
+        if($prefecture_search !== "0") {
+            $prefecture_search_condition = Prefecture::find($prefecture_search)->prefecture;
+        } else {
+            $prefecture_search_condition = '指定なし';
+        }
+        if($company_search  !== "0") {
+            $company_search_condition = CompanyType::find($company_search)->company_type;
+        } else {
+            $company_search_condition = '指定なし';
+        }
+        if($phase_search !== "0") {
+            $phase_search_condition = Phase::find($phase_search)->phase;
+        } else {
+            $phase_search_condition = '指定なし';
+        }
+
+        return [
+            'prefecture' => $prefecture_search_condition,
+            'company' => $company_search_condition,
+            'phase' => $phase_search_condition,
+        ];
     }
 }
